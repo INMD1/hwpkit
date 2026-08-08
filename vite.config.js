5c2fe639f7a10a37600c0019b93f6952196ccfbd
INMD1 <lyw514549@gmail.com>
2026-08-08 12:27:23 +0900

    실수

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
-    allowedHosts: ['https://hwpkitdev.devmultid.org/'],  
+    allowedHosts: ['hwpkitdev.devmultid.org/'],  
   },
 });
